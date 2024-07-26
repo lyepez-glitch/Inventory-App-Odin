a38a10241cf49fc2381beb492dfe0535ab2c841c
Lucas Yepez <lucasyepez99@gmail.com>
2024-07-26 09:52:07 -0500

    got basic seeding to work

--- seed.js
+++ seed.js
@@ -17,39 +17,42 @@ async function main() {
         const response = await axios.get('https://dummyjson.com/products?limit=100');
         console.log('response', response.data);
         const products = response.data["products"];
-        let count = 0;
+
         // console.log(Array.isArray(products))
         const queries = products.map(async(product) => {
-            count++;
+
 
             const product_id = product.id;
-            const product_name = product.title;
-            const price = product.price;
-            const brand = product.brand;
-            const category = product.category;
-            const review = product.reviews[0];
-            // console.log(count, product)
-            let qry =
+            const product_name = product.title || "none";
+            const price = parseInt(product.price) || 0;
+            const brand = product.brand || "none";
+            const category = product.category || "none";
+            const review = product.reviews[0] || "none";
+            // console.log(product_id, product)
+
+
+            let qry = `INSERT INTO categories(id,name)
+                VALUES ($1,$2);`
+            let vals = [product_id, category]
+            await client.query(qry, vals);
+
+            qry = `INSERT INTO brands(id,name)
+                VALUES ($1,$2);`
+
+            await client.query(qry, [product_id, brand]);
+            qry = `INSERT INTO reviews(id, review_txt)
+                VALUES
+                    ($1,$2);`
+            await client.query(qry, [product_id, review]);
+
+            qry =
                 `INSERT INTO products(id, name, price, brand, category, review)
                 VALUES
                     ($1,$2,$3,$4,$5,$6);
                  `
-            let vals = [count, product_name, price, count, count, count]
+            console.log("prod id", product_id)
+            vals = [product_id, product_name, price, product_id, product_id, product_id]
             await client.query(qry, vals);
-
-            qry = `INSERT INTO categories(name)
-                VALUES ($1);`
-            vals = [category]
-            await client.query(qry, vals);
-
-            qry = `INSERT INTO brands(name)
-                VALUES ($1);`
-
-            await client.query(qry, [brand]);
-            qry = `INSERT INTO reviews(id, review_txt)
-                VALUES
-                    ($1), ($2)`
-            await client.query(qry, [product_id, review]);
         })
 
         await Promise.all(queries);
