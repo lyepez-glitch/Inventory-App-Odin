0d1bb5356dd99a8ec837bbb31a6db744966f6e89
Lucas Yepez <lucasyepez99@gmail.com>
2024-07-28 01:58:17 -0500

    bug fix

--- seed.js
+++ seed.js
@@ -1,79 +1,79 @@
-// #! /usr/bin/env node
-require('dotenv').config();
-const axios = require('axios');
-const { Client } = require("pg");
-const db = require('./db/categoryQueries');
-let SQL = '';
+// // #! /usr/bin/env node
+// require('dotenv').config();
+// const axios = require('axios');
+// const { Client } = require("pg");
+// const db = require('./db/categoryQueries');
+// let SQL = '';
 
-async function main() {
-    console.log("seeding...");
-    const client = new Client({
-        connectionString: `postgresql://${process.env.ROLE}:${process.env.PASSWORD}@localhost:${process.env.PORT}/${process.env.DATABASE}`,
-    });
-    await client.connect();
+// async function main() {
+//     console.log("seeding...");
+//     const client = new Client({
+//         connectionString: `postgresql://${process.env.ROLE}:${process.env.PASSWORD}@localhost:${process.env.PORT}/${process.env.DATABASE}`,
+//     });
+//     await client.connect();
 
-    console.log("done");
-    try {
+//     console.log("done");
+//     try {
 
-        const response = await axios.get('https://dummyjson.com/products?limit=100');
+//         const response = await axios.get('https://dummyjson.com/products?limit=100');
 
-        const products = response.data["products"];
-
-
-        const queries = products.map(async(product) => {
-            const foundCat = await db.findCatByName(product.category);
+//         const products = response.data["products"];
 
 
-            let cat;
-            let cat_id;
-
-            let qry = `SELECT id FROM categories WHERE name=$1`
-
-            cat = await client.query(qry, [product.category]);
-
-            if (cat.rows.length === 0) {
-                qry = `INSERT INTO categories(name)
-                   VALUES ($1) RETURNING id;`
-
-                cat = await client.query(qry, [product.category]);
-                cat_id = cat.rows[0].id;
-
-            } else {
-
-                cat_id = cat.rows[0].id;
-
-            }
-            console.log(' cat.rows', cat.rows)
+//         const queries = products.map(async(product) => {
+//             const foundCat = await db.findCatByName(product.category);
 
 
-            const product_name = product.title;
-            const price = parseInt(product.price) || 0;
+//             let cat;
+//             let cat_id;
 
-            qry =
-                `INSERT INTO products(name, price, category)
-                VALUES
-                    ($1,$2,$3) RETURNING id;
-                 `
-            vals = [product_name, price, product.category]
-            await client.query(qry, vals);
+//             let qry = `SELECT id FROM categories WHERE name=$1`
 
-        })
+//             cat = await client.query(qry, [product.category]);
 
-        await Promise.all(queries);
+//             if (cat.rows.length === 0) {
+//                 qry = `INSERT INTO categories(name)
+//                    VALUES ($1) RETURNING id;`
+
+//                 cat = await client.query(qry, [product.category]);
+//                 cat_id = cat.rows[0].id;
+
+//             } else {
+
+//                 cat_id = cat.rows[0].id;
+
+//             }
+//             console.log(' cat.rows', cat.rows)
+
+
+//             const product_name = product.title;
+//             const price = parseInt(product.price) || 0;
+
+//             qry =
+//                 `INSERT INTO products(name, price, category)
+//                 VALUES
+//                     ($1,$2,$3) RETURNING id;
+//                  `
+//             vals = [product_name, price, product.category]
+//             await client.query(qry, vals);
+
+//         })
+
+//         await Promise.all(queries);
 
 
 
 
-    } catch (error) {
-        console.error('Error fetching products:', error);
-    } finally {
-        try {
-            await client.end();
-        } catch (e) {
-            console.log('err', e.message)
-        }
+//     } catch (error) {
+//         console.error('Error fetching products: ', error);
+//     } finally {
+//         try {
+//             await client.end();
+//         } catch (e) {
+//             console.log('err', e.message)
+//         }
 
-    }
-}
+//     }
+// }
 
-main();
+// main();
